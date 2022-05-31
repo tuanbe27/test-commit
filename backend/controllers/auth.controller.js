554bdb4377b4f25838974b6f9f18f877ac0ed0ea
tuanbe27 <anhtuandb06@gmail.com>
2022-05-31 16:14:07 +0700

    add gitignore

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,13 @@
-const formidable = require("formidable");
-const validator = require("validator");
-const registerModel = require("../models/auth.model");
-const fs = require("fs");
-const bcrypt = require("bcrypt");
-const path = require("path");
+const formidable = require('formidable');
+const validator = require('validator');
+const registerModel = require('../models/auth.model');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const path = require('path');
 
 const isFileValid = (file) => {
-  const type = file.originalFilename.split(".").pop();
-  const validTypes = ["jpg", "jpeg", "png"];
+  const type = file.originalFilename.split('.').pop();
+  const validTypes = ['jpg', 'jpeg', 'png'];
   if (validTypes.indexOf(type) === -1) {
     return false;
   }
@@ -16,7 +16,7 @@ const isFileValid = (file) => {
 
 module.exports.userRegister = (req, res) => {
   const form = new formidable.IncomingForm();
-  const uploadFolder = path.join(__dirname, "../public", "image");
+  const uploadFolder = path.join(__dirname, '../public', 'image');
   //   form.multiples = true;
   form.maxFileSize = 50 * 1024 * 1024; // 5MB
   form.uploadDir = uploadFolder;
@@ -32,51 +32,51 @@ module.exports.userRegister = (req, res) => {
     if (!isValid) {
       // throes error if file isn't valid
       return res.status(400).json({
-        status: "Fail",
-        message: "The file type is not a valid type",
+        status: 'Fail',
+        message: 'The file type is not a valid type',
       });
     }
 
     if (!username) {
-      error.push("Please provide your username");
+      error.push('Please provide your username');
     }
     if (!email) {
-      error.push("Please provide your email");
+      error.push('Please provide your email');
     }
     if (email && !validator.default.isEmail(email)) {
-      error.push("Please provide your username");
+      error.push('Please provide your username');
     }
     if (!password) {
-      error.push("Please provide your password");
+      error.push('Please provide your password');
     }
     if (!confirmPassword) {
-      error.push("Please provide your confirm password");
+      error.push('Please provide your confirm password');
     }
     if (password && confirmPassword && password !== confirmPassword) {
-      error.push("Your password and confirm password are not same");
+      error.push('Your password and confirm password are not same');
     }
     if (password && password.length < 6) {
-      error.push("Please provide password must be greater than 6 characters");
+      error.push('Please provide password must be greater than 6 characters');
     }
     if (Object.keys(files).length === 0) {
-      error.push("Please provide user image");
+      error.push('Please provide user image');
     }
     if (error.length > 0) {
       return res.status(400).json({
-        status: "Fail",
+        status: 'Fail',
         errors: error,
       });
     }
 
     const getImageName = files.image.originalFilename;
-    const newImageName = new Date().getTime() + "_" + getImageName;
+    const newImageName = new Date().getTime() + '_' + getImageName;
 
     try {
       const checkUser = await registerModel.findOne({ email: email });
 
       if (checkUser) {
         return res.status(404).json({
-          error: { errorMessage: "Your email has aleady exists" },
+          error: { errorMessage: 'Your email has aleady exists' },
         });
       }
 
@@ -88,13 +88,13 @@ module.exports.userRegister = (req, res) => {
         username,
         email,
         password: await bcrypt.hash(password, 10),
-        image: image.originalFilename,
+        image: '/image/' + image.originalFilename,
       });
       res.status(200).json(newUser);
     } catch (error) {
       console.log(error);
       res.status(500).json({
-        status: "Fail",
+        status: 'Fail',
         errorMessage: error.message,
       });
     }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,6 @@
-const router = require("express").Router();
-const { userRegister } = require("../controllers/auth.controller");
+const router = require('express').Router();
+const { userRegister } = require('../controllers/auth.controller');
 
-router.post("/user-register", userRegister);
+router.post('/user-register', userRegister);
 
 module.exports = router;
